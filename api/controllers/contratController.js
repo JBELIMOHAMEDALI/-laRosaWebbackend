7460ad577c4ba6aa25e1aa80fcd8bc2c73200694
minercool <zghidig@gmail.com>
2021-09-11 17:37:53 +0100

    new

--- api/controllers/contratController.js
+++ api/controllers/contratController.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { query } = require("express");
 const connection = require("../../database/db");
 const { route } = require("./agentController");
 module.exports = router;
@@ -38,17 +39,17 @@ router.get("/get/:id", async (req, res) => {
 
 router.delete("/delete/:id", async (req, res) => {
   // try {
-    connection.query(
-      "DELETE FROM contrat WHERE id = ?",
-      req.params.id,
-      (error, rows, fields) => {
-        if (rows.affectedRows != 0) {
-          res.status(200).json({msg:"deleted succesfully"});
-        } else {
-          res.status(404).json({msg:"id not found"});
-        }
+  connection.query(
+    "DELETE FROM contrat WHERE id = ?",
+    req.params.id,
+    (error, rows, fields) => {
+      if (rows.affectedRows != 0) {
+        res.status(200).json({ msg: "deleted succesfully" });
+      } else {
+        res.status(404).json({ msg: "id not found" });
       }
-    );
+    }
+  );
   // } catch (error) {
   //   console.error(error.message);
   //   res.status(500).send("server error");
@@ -57,37 +58,37 @@ router.delete("/delete/:id", async (req, res) => {
 
 router.post("/post", async (req, res) => {
   // try {
-    connection.query(
-      "INSERT INTO contrat(numero,client,article,qte,montant,avance,datecontrat,premiereecheance,nbrecheance,mensualite,2annuite,2mensualite,commission_vendeur,recouvreur,remarque,chef,comm,statucontrat,active) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-      [
-        req.body.numero,
-        req.body.client,
-        req.body.article,
-        req.body.qte,
-        req.body.montant,
-        req.body.avance, 
-        req.body.datecontrat,
-        req.body.premiereecheance,
-        req.body.nbrecheance,
-        req.body.mensualite,
-        req.body.annuite2,
-        req.body.mensualite2,
-        req.body.commission_vendeur,
-        req.body.recouvreur,
-        req.body.remarque,
-        req.body.chef,
-        req.body.comm,
-        req.body.statucontrat,
-        req.body.active,
-      ],
-      (error, rows, fields) => {
-        if (rows.affectedRows != 0) {
-          res.status(200).send(rows);
-        } else {
-          res.status(500).send(error);
-        }
+  connection.query(
+    "INSERT INTO contrat(numero,client,article,qte,montant,avance,datecontrat,premiereecheance,nbrecheance,mensualite,2annuite,2mensualite,commission_vendeur,recouvreur,remarque,chef,comm,statucontrat,active) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+    [
+      req.body.numero,
+      req.body.client,
+      req.body.article,
+      req.body.qte,
+      req.body.montant,
+      req.body.avance,
+      req.body.datecontrat,
+      req.body.premiereecheance,
+      req.body.nbrecheance,
+      req.body.mensualite,
+      req.body.annuite2,
+      req.body.mensualite2,
+      req.body.commission_vendeur,
+      req.body.recouvreur,
+      req.body.remarque,
+      req.body.chef,
+      req.body.comm,
+      req.body.statucontrat,
+      req.body.active,
+    ],
+    (error, rows, fields) => {
+      if (rows.affectedRows != 0) {
+        res.status(200).send(rows);
+      } else {
+        res.status(500).send(error);
       }
-    );
+    }
+  );
   // } catch (error) {
   //   console.error(error.message);
   //   res.status(500).send("server error");
@@ -118,7 +119,7 @@ router.patch("/update/:id", async (req, res) => {
         req.body.comm,
         req.body.statucontrat,
         req.body.active,
-        req.params.id
+        req.params.id,
       ],
       (error, rows, fields) => {
         if (rows.affectedRows != 0) {
@@ -134,14 +135,10 @@ router.patch("/update/:id", async (req, res) => {
   }
 });
 
-
-
-
-
 router.get("/getInfoContra/:num_contra", async (req, res) => {
   connection.query(
     "select contrat.*,sum(payment.PaiedAmmount)+contrat.avance as payeyedsom from contrat,payment WHERE payment.Contrat=? and contrat.numero = ?",
-    [req.params.num_contra,req.params.num_contra],
+    [req.params.num_contra, req.params.num_contra],
     (error, rows, fields) => {
       if (rows.length > 0) {
         res.status(200).send(rows[0]);
@@ -150,14 +147,12 @@ router.get("/getInfoContra/:num_contra", async (req, res) => {
       }
     }
   );
-
 });
-
 
 router.get("/getContraBayNumro/:num_contra", async (req, res) => {
   connection.query(
     "SELECT * from contrat WHERE contrat.numero= ?",
-    [req.params.num_contra,req.params.num_contra],
+    [req.params.num_contra, req.params.num_contra],
     (error, rows, fields) => {
       if (rows.length > 0) {
         res.status(200).send(rows[0]);
@@ -166,6 +161,44 @@ router.get("/getContraBayNumro/:num_contra", async (req, res) => {
       }
     }
   );
+});
 
+
+router.get("/stat-contrat", async (req, res) => {
+  try {
+
+
+
+    connection.query(
+      "SELECT * FROM contrat WHERE (article = ? OR (? LIKE '')) AND (montant = ? OR (? IS NULL) OR (? LIKE '')) AND (chef = ? OR (? LIKE '')) AND (client = ? OR (? LIKE '')) AND (recouvreur = ? OR (? LIKE '')) AND (comm = ? OR (? LIKE '')) AND (statucontrat = ? OR (? LIKE ''))",
+      [req.body.article,
+        req.body.article,
+        req.body.montant,
+        req.body.montant,
+        req.body.montant,
+        req.body.chef,
+        req.body.chef,
+        req.body.client,
+        req.body.client,
+        req.body.recouvreur,
+        req.body.recouvreur,
+        req.body.comm,
+        req.body.comm,
+        req.body.status,
+        req.body.status,
+      
+      ],
+      (error, rows, fields) => {
+        if (rows.length > 0) {
+          res.status(200).send(rows);
+        } else {
+          res.status(404).send("contract not found");
+        }
+      }
+    );
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("server error");
+  }
 });
 //
