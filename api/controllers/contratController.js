4242901dda8d80dddefe48c69e3950d443b4e8b6
minercool <zghidig@gmail.com>
2021-09-12 15:29:59 +0100

    update

--- api/controllers/contratController.js
+++ api/controllers/contratController.js
@@ -165,33 +165,111 @@ router.get("/getContraBayNumro/:num_contra", async (req, res) => {
 
 router.get("/stat-contrat", async (req, res) => {
   try {
-    connection.query(
-      "SELECT contrat.numero,contrat.client,contrat.article,contrat.qte,contrat.datecontrat,contrat.premiereecheance,contrat.statucontrat FROM contrat, clients WHERE (contrat.article = ? OR (? LIKE '')) AND (contrat.montant = ? OR (? IS NULL) OR (? LIKE '')) AND (contrat.chef = ? OR (? LIKE '')) AND (contrat.client = ? OR (? LIKE '')) AND (contrat.recouvreur = ? OR (? LIKE '')) AND (contrat.comm = ? OR (? LIKE '')) AND (contrat.statucontrat = ? OR (? LIKE ''))",
-      [
-        req.body.article,
-        req.body.article,
-        req.body.montant,
-        req.body.montant,
-        req.body.montant,
-        req.body.chef,
-        req.body.chef,
-        req.body.client,
-        req.body.client,
-        req.body.recouvreur,
-        req.body.recouvreur,
-        req.body.comm,
-        req.body.comm,
-        req.body.status,
-        req.body.status,
-      ],
-      (_error, rows, fields) => {
-        if (rows.length > 0) {
-          res.status(200).json(rows);
-        } else {
-          res.status(404).send("contract not found");
+    if (
+      req.body.client == "" &&
+      req.body.region != "" &&
+      req.body.article == "" &&
+      (req.body.montant == null || req.body.montant == "") &&
+      req.body.chef == "" &&
+      req.body.client == "" &&
+      req.body.recouvreur == "" &&
+      req.body.comm == "" &&
+      req.body.status == ""
+    ) {
+      connection.query(
+        "SELECT contrat.numero,contrat.client,contrat.article,contrat.qte,contrat.datecontrat,contrat.premiereecheance,contrat.statucontrat FROM contrat,clients WHERE (contrat.client = clients.nom) AND (clients.region = ?)",
+        [req.body.region],
+        (error, rows, fields) => {
+          if (rows.length > 0) {
+            res.status(200).send(rows);
+          } else {
+            res.status(404).send("no contrat found");
+          }
         }
-      }
-    );
+      );
+    }
+    else if (
+    req.body.client == "" &&
+    req.body.region == "" &&
+    req.body.zone != "" &&
+    req.body.article == "" &&
+    (req.body.montant == null || req.body.montant == "") &&
+    req.body.chef == "" &&
+    req.body.client == "" &&
+    req.body.recouvreur == "" &&
+    req.body.comm == "" &&
+    req.body.status == ""){
+      connection.query(
+        "SELECT contrat.numero,contrat.client,contrat.article,contrat.qte,contrat.datecontrat,contrat.premiereecheance,contrat.statucontrat FROM contrat,clients WHERE (contrat.client = clients.nom) AND (clients.zone = ?)",
+        [req.body.zone],
+        (error, rows, fields) => {
+          if (rows.length > 0) {
+            res.status(200).send(rows);
+          } else {
+            res.status(404).send("no contrat found");
+          }
+        }
+      );
+
+    }
+    else if(
+      req.body.client == "" &&
+      req.body.region == "" &&
+      req.body.article == "" &&
+      (req.body.montant == null || req.body.montant == "") &&
+      req.body.chef == "" &&
+      req.body.client == "" &&
+      req.body.recouvreur == "" &&
+      req.body.comm == "" &&
+      req.body.status == ""
+    ){
+      connection.query("SELECT contrat.numero,contrat.client,contrat.article,contrat.qte,contrat.datecontrat,contrat.premiereecheance,contrat.statucontrat FROM contrat",(error , rows ,fields)=>{
+        if(rows.length > 0){
+          res.status(200).send(rows)
+        }else{
+          res.status(404).send('not found');
+        }
+      })
+    }
+     
+    else{
+      connection.query(
+        "SELECT contrat.numero,contrat.client,contrat.article,contrat.qte,contrat.datecontrat,contrat.premiereecheance,contrat.statucontrat FROM contrat,clients WHERE (((? = clients.nom) AND (clients.region = ?) AND (? NOT LIKE '')) OR (? LIKE '')) AND (((? = clients.nom) AND (clients.zone = ?) AND (? NOT LIKE '')) OR (? LIKE '')) AND(contrat.article = ? OR (? LIKE '')) AND (contrat.montant = ? OR (? IS NULL) OR (? LIKE '')) AND (contrat.chef = ? OR (? LIKE '')) AND (contrat.client = ? OR (? LIKE '')) AND (contrat.recouvreur = ? OR (? LIKE '')) AND (contrat.comm = ? OR (? LIKE '')) AND (contrat.statucontrat = ? OR (? LIKE '')) ",
+        [
+          req.body.client,
+          req.body.region,
+          req.body.client,
+          req.body.region,
+          req.body.client,
+          req.body.zone,
+          req.body.client,
+          req.body.zone,
+          req.body.article,
+          req.body.article,
+          req.body.montant,
+          req.body.montant,
+          req.body.montant,
+          req.body.chef,
+          req.body.chef,
+          req.body.client,
+          req.body.client,
+          req.body.recouvreur,
+          req.body.recouvreur,
+          req.body.comm,
+          req.body.comm,
+          req.body.status,
+          req.body.status,
+        ],
+        (error, rows, fields) => {
+          if (rows.length > 0) {
+            res.status(200).send(rows);
+          } else {
+            res.status(404).send("contract not found");
+          }
+        }
+      );
+    }
+   
   } catch (error) {
     console.error(error.message);
     res.status(500).send("server error");
